70cd6f7253102068017a0c1846638ec2c34c9f20
Boychild-codes <stephenjiru@gmail.com>
2025-03-22 20:01:02 +0300

    remove love emoji

--- script.js
+++ script.js
@@ -25,4 +25,38 @@ var tablinks = document.getElementsByClassName("tab-links");
     }
 
 
+
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        event.preventDefault(); // Prevent the form from submitting the traditional way
+      
+        const form = event.target;
+        const formData = {
+          name: form.name.value,
+          email: form.email.value,
+          message: form.message.value,
+        };
+      
+        const status = document.getElementById('form-status');
+      
+        // Replace with your Google Apps Script Web App URL
+        const scriptURL = 'AKfycbxyRZ1bVHQMgbsOHoSNL0MhTjcnEPNRQ-_2cvKdyFxP';
+      
+        fetch(scriptURL, {
+          method: 'POST',
+          body: JSON.stringify(formData),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+          .then(response => response.text())
+          .then(data => {
+            status.innerHTML = 'Message sent successfully!';
+            form.reset();
+          })
+          .catch(error => {
+            status.innerHTML = 'Oops! There was a problem sending your message.';
+          });
+      });
+
+
     
