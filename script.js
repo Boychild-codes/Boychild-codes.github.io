e424b2666cbac030767894b53dba6c37e4cbad50
Boychild-codes <stephenjiru@gmail.com>
2025-04-04 07:31:28 +0300

    Some more responsive menu

--- script.js
+++ script.js
@@ -13,15 +13,8 @@ var tablinks = document.getElementsByClassName("tab-links");
         }
 
         //               Menu     //
-        function showSidebar() {
-            const sidebar = document.querySelector('.sidebar');
-            sidebar.style.display = 'flex';
-        }
-        
-        function hideSidebar() {
-            const sidebar = document.querySelector('.sidebar');
-            sidebar.style.display = 'none';
-        }
+
+   
 
 //               Form     //
 // Contact Form Submission //
